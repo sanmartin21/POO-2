1aa75ad2bf151fc023b3af16e05dcba14e9ff346
sanmartin21 <jvsanmartin@yahoo.com.br>
2022-08-17 10:15:59 -0300

    Criando Repositório

---
+++ atendimentoMedico/src/atendimentoMedico/Medico.java
@@ -0,0 +1,10 @@
+package atendimentoMedico;
+
+import java.util.Date;
+
+public class Medico extends Pessoa{
+	
+	public Medico(String nome, Date dataNascimento) {
+		super(nome, dataNascimento);
+	}
+}
